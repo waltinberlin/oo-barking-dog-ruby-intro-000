e26a5d5faeffdc32d227998086ed985cb23294cb
Walter Minehuber <minehuber@gmail.com>
2017-09-30 09:10:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,14 @@
 # Your code goes here!
 class Dog
+  def initiate(name)
+    @name = name
+  end
+
   def name=(name)
     @name = name
+  end
+  def name
+    @name
   end
   def bark
     puts "Whoof!"
